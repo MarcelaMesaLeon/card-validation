2c97a0a5542a63fe6cfbbdf5a87adc42eb96982b
MarcelaMesaLeon <mardie05@hotmail.com>
2023-02-10 19:36:20 -0500

    Se agrega funcionalidad para reversar string y convertir a array numerico.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import validator from './validator.js';
 
+const form = document.querySelector("form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const userNum = document.getElementById("creditCardNum").value;
+  validator.isValid(userNum);
+});
 
-console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,17 @@
-
 const validator = {
-  isValid: () => {
-
+  isValid: (userNum) => {
+    creditCardReverse(userNum);
   },
-  maskify: () => {
-
-  }
+  maskify: () => {},
 };
+
+
+
+function creditCardReverse(userNum) {
+  let arrayString = Array.from(userNum);
+  arrayString = arrayString.reverse();
+  const arrayNum = arrayString.map((num) => Number(num));
+  console.log(arrayNum);
+}
 
 export default validator;
